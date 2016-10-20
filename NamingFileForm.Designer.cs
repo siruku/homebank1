229f306d899f62da13ba2c0e859bceb92e7f1676
seiyaSasaki <siruku6@hotmail.com>
2016-10-20 22:24:52 +0900

    readd omission of previous commit

---
+++ NamingFileForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace HomeBank1
+{
+    partial class NamingFileForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.CompleteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(54, 33);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(129, 19);
+            this.NameBox.TabIndex = 0;
+            // 
+            // CompleteButton
+            // 
+            this.CompleteButton.Location = new System.Drawing.Point(12, 58);
+            this.CompleteButton.Name = "CompleteButton";
+            this.CompleteButton.Size = new System.Drawing.Size(75, 23);
+            this.CompleteButton.TabIndex = 1;
+            this.CompleteButton.Text = "決定";
+            this.CompleteButton.UseVisualStyleBackColor = true;
+            this.CompleteButton.Click += new System.EventHandler(this.CompleteButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ノートの名前を決めてください。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "名前：";
+            // 
+            // CancelButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(108, 58);
+            this.ExitButton.Name = "CancelButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "キャンセル";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // NamingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 91);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CompleteButton);
+            this.Controls.Add(this.NameBox);
+            this.Name = "NamingForm";
+            this.Text = "新規ノート作成";
+            this.Load += new System.EventHandler(this.NamingForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button CompleteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
---
+++ NamingFileForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HomeBank1
+{
+    public partial class NamingFileForm : Form {
+        public NamingFileForm() {
+            InitializeComponent();
+        }
+
+        private void NamingForm_Load(object sender, EventArgs e){
+            this.MaximizeBox = this.MinimizeBox = false;
+        }
+        private void CompleteButton_Click(object sender, EventArgs e){
+            if (NameBox.Text == "") { return; }
+            if (ComMthd.RaiseQuestion("新しく作るノートの名前は　" + NameBox.Text + "　でよろしいですか？")
+                == DialogResult.Yes){
+                //SavedData.activeFilePass = 
+                //    Path.Combine(Application.StartupPath, NameBox.Text + ".xml");
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+        private void ExitButton_Click(object sender, EventArgs e){
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
